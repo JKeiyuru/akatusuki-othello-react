f58a1f64ae0e5429345d86bc08769f79a32c977d
Jane-Njoroge <njorogejwangui@gmail.com>
2024-11-13 21:03:00 +0300

    added board

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './GameBoard.css';
 
-function GameBoard({ boardState }) {
-  return (
-    <div className="game-board">
-      {boardState.map((row, rowIndex) => (
-        row.map((cell, cellIndex) => (
-          <div key={${rowIndex}-${cellIndex}} className="cell">
-            <div className={disc ${cell === 'B' ? 'black' : cell === 'W' ? 'white' : ''}}></div>
+const GameBoard = ({ boardState }) => (
+  <div className="game-board">
+    <div className="board-grid">
+      {boardState.map((row, i) =>
+        row.map((cell, j) => (
+          <div key={${i}-${j}} className="cell">
+            <div className={disc ${cell === 'B' ? 'black' : cell === 'W' ? 'white' : ''}} />
           </div>
         ))
-      ))}
+      )}
     </div>
-  );
-}
+  </div>
+);
 
 export default GameBoard;
