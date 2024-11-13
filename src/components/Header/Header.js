0d3356d60ebba4b799f6e90ea930bd178eb3be7f
Joseph Keiyuru <josephkeiyuru1@gmail.com>
2024-11-13 08:33:57 +0300

    Folder structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+// src/components/Header/Header.js
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1>Othello Tutorial</h1>
+  </header>
+);
+
+export default Header;
